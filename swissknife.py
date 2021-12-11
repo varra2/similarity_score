36f4c80b0fc7f66dc8683c2c3e74940fdb4ce105
unknown <varra2@yandex.ru>
2021-12-11 17:45:12 +0300

    tests added

--- swissknife.py
+++ swissknife.py
@@ -5,8 +5,6 @@ from django.core.management import execute_from_command_line
 from django.http import JsonResponse
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-from django.test import Client
-import unittest
 import json
 import spacy
 from langdetect import detect
@@ -52,28 +50,6 @@ def requestView(request):
 urlpatterns = [
     path("similar-recognition", requestView, name='similar-recognition')
     ]
-
-#some tests... they don't work though
-
-class SimpleTest(unittest.TestCase):
-
-    def test_identical_texts_scored_1(self):        
-        self.client = Client()
-        response = self.client.post('/requestpage', json.dumps({'text1': 'Hello, world!', 'text2': 'Hello, world!'}), content_type="application/json")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()["Similarity score:"], 1)
-
-    def test_works_for_russian(self):
-        self.client = Client()
-        response = self.client.post('/requestpage', json.dumps({'text1': 'Здравствуй, мир!', 'text2': 'Здравствуй, мир!'}), content_type="application/json")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()["Similarity score:"], 1)
-
-    def test_different_languages_scored_0(self):
-        self.client = Client()
-        response = self.client.post('/requestpage', json.dumps({'text1': 'Hello, world!', 'text2': 'Здравствуй, мир!'}), content_type="application/json")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()["Similarity score:"], 0)
 
 #settings and manage
 
